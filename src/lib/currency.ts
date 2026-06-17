fa637fa399697ebb5e4cb1d44088324a80225351
memzhroyale <memzhroyale@gmail.com>
2026-06-18 04:35:29 +0800

    feat: add PHP currency

--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -2,6 +2,7 @@ export const currencies = [
   { code: "USD", symbol: "$", name: "US Dollar" },
   { code: "EUR", symbol: "€", name: "Euro" },
   { code: "GBP", symbol: "£", name: "British Pound" },
+  { code: "PHP", symbol: "₱", name: "Philippine Peso" },
   { code: "JPY", symbol: "¥", name: "Japanese Yen" },
   { code: "CAD", symbol: "CA$", name: "Canadian Dollar" },
   { code: "AUD", symbol: "A$", name: "Australian Dollar" },
